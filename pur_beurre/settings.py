99066fd3d558beace5537c5b855b51c6839319d7
MagicRom06 <romainhenry60@gmail.com>
2021-04-27 21:14:30 +0200

    rollback google auth

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -162,7 +162,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
-SITE_ID = 1
+SITE_ID = 2
 
 AUTHENTIFICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
