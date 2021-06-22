d3780b49d6600e17e1153c74eaeed56e8ca6c532
MagicRom06 <romainhenry60@gmail.com>
2021-06-23 01:00:53 +0200

    fix travis config

--- home_page/tests.py
+++ home_page/tests.py
@@ -14,19 +14,3 @@ class HomePageViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Du gras, oui, mais de qualité !")
         self.assertTemplateUsed(response, 'home_page/home.html')
-
-"""
-class SearchFormTest(LiveServerTestCase):
-    """
-    # live testing search form from home page
-    """
-    def test_search_form(self):
-        selenium = webdriver.Chrome(ChromeDriverManager().install())
-        selenium.get('http://127.0.0.1:8000/')
-        search = selenium.find_element_by_id('search')
-        submit = selenium.find_element_by_id('submit_button')
-        search.send_keys('nutella')
-        submit.send_keys(Keys.RETURN)
-        assert 'Resultats' in selenium.page_source
-        assert 'Nutella' in selenium.page_source
-"""
