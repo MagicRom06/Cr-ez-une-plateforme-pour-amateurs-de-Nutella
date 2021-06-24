ebb7c083a76126102f3cf13cedcc7b88cad2c8cb
MagicRom06 <romainhenry60@gmail.com>
2021-06-24 22:04:31 +0200

    configure travis for selenium

--- home_page/tests.py
+++ home_page/tests.py
@@ -1,7 +1,6 @@
 from django.test import LiveServerTestCase, TestCase
 from django.urls import reverse
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -22,10 +21,6 @@ class SearchFormTest(LiveServerTestCase):
     live testing search form from home page
     """
     def test_search_form(self):
-        chrome_options = Options()
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--disable-dev-shm-usage')
         selenium = webdriver.Chrome(ChromeDriverManager().install())
         selenium.get('http://127.0.0.1:8000/')
         search = selenium.find_element_by_id('search')
