259b397a7b22f72c491ab81e7605beafe624561b
MagicRom06 <romainhenry60@gmail.com>
2021-06-25 21:24:29 +0200

    configure travis for selenium

--- home_page/tests.py
+++ home_page/tests.py
@@ -1,11 +1,9 @@
-from django.test import LiveServerTestCase, TestCase
+from django.test import TestCase
 from django.urls import reverse
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.chrome.options import Options
 
 
 class HomePageViewTest(TestCase):
@@ -26,21 +24,24 @@ class SearchFormTest(StaticLiveServerTestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        chromeOptions = webdriver.ChromeOptions() 
+        chromeOptions = webdriver.ChromeOptions()
         chromeOptions.add_experimental_option(
-            "prefs", 
+            "prefs",
             {"profile.managed_default_content_settings.images": 2}
-        ) 
-        chromeOptions.add_argument("--no-sandbox") 
-        chromeOptions.add_argument("--disable-setuid-sandbox") 
+        )
+        chromeOptions.add_argument("--no-sandbox")
+        chromeOptions.add_argument("--disable-setuid-sandbox")
         chromeOptions.add_argument("--remote-debugging-port=9222")
-        chromeOptions.add_argument("--disable-dev-shm-using") 
-        chromeOptions.add_argument("--disable-extensions") 
-        chromeOptions.add_argument("--disable-gpu") 
-        chromeOptions.add_argument("start-maximized") 
+        chromeOptions.add_argument("--disable-dev-shm-using")
+        chromeOptions.add_argument("--disable-extensions")
+        chromeOptions.add_argument("--disable-gpu")
+        chromeOptions.add_argument("start-maximized")
         chromeOptions.add_argument("disable-infobars")
-        chromeOptions.add_argument(r"user-data-dir=.\cookies\\test") 
-        cls.selenium = webdriver.Chrome(ChromeDriverManager().install(), options=chromeOptions)
+        chromeOptions.add_argument(r"user-data-dir=.\cookies\\test")
+        cls.selenium = webdriver.Chrome(
+            ChromeDriverManager().install(),
+            options=chromeOptions
+        )
         cls.selenium.implicitly_wait(10)
 
     @classmethod
