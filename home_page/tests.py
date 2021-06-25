061e0761f588048c01179fe7de719ec922ed2b16
MagicRom06 <romainhenry60@gmail.com>
2021-06-25 14:50:30 +0200

    configure travis for selenium

--- home_page/tests.py
+++ home_page/tests.py
@@ -3,6 +3,8 @@ from django.urls import reverse
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
 
 
 class HomePageViewTest(TestCase):
@@ -16,16 +18,25 @@ class HomePageViewTest(TestCase):
         self.assertTemplateUsed(response, 'home_page/home.html')
 
 
-class SearchFormTest(LiveServerTestCase):
+class SearchFormTest(StaticLiveServerTestCase):
     """
     live testing search form from home page
     """
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = webdriver.Chrome(ChromeDriverManager().install())
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
     def test_search_form(self):
-        selenium = webdriver.Chrome(ChromeDriverManager().install())
-        selenium.get('http://127.0.0.1:8000/')
-        search = selenium.find_element_by_id('search')
-        submit = selenium.find_element_by_id('submit_button')
+        self.selenium.get(self.live_server_url)
+        search = self.selenium.find_element_by_id('search')
+        submit = self.selenium.find_element_by_id('submit_button')
         search.send_keys('nutella')
         submit.send_keys(Keys.RETURN)
-        assert 'Resultats' in selenium.page_source
-        assert 'Nutella' in selenium.page_source
+        assert 'Resultats' in self.selenium.page_source
