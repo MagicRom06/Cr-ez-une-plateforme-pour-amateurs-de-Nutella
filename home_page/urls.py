2b1a11208b973b8177cb13a6e8a7fef8937e7b0e
MagicRom06 <romainhenry60@gmail.com>
2021-07-26 15:09:35 +0200

    configure cache with redis and add cache for home and search

--- home_page/urls.py
+++ home_page/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from . import views
 from .views import HomePageView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
+    path('', cache_page(60 * 15)(HomePageView.as_view()), name='home'),
     path('newsletter/', views.newsletterPage, name='newsletter')
 ]
